7d5e1de9ce2a466abe40519c9ceb70a4a05bf8ac
topsy <topsytrabajo@gmail.com>
2026-05-16 18:17:45 -0300

    comentarios aclaratorios de la clase ValidateModel

--- app/base_models.py
+++ app/base_models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+#ValidableModel herda de models.Model, cualquier clase que herede de esta es un modelo válido de Django
 class ValidableModel(models.Model):
+    #Meta define metadatos del modelo, en este caso define que es una clase abstracta, por lo tanto Django no va a crear una tabla de ella
     class Meta:
         abstract = True
 
+    #convierte el metodo en uno de clase, por ej llamandolo con Ferias.validate()...
     @classmethod
+    #cls es clase, kwargs son keyword arguments 
     def validate(cls, **kwargs) -> list[str]:
         """
         Debe ser sobrescrito en el modelo de negocio.
@@ -18,19 +22,22 @@ class ValidableModel(models.Model):
         """
         Sigue la estructura exacta del ejemplo: valida y si no hay errores, crea.
         """
+        #primero valida que los argumentos pasados en la creacion sean correctos
         errors = cls.validate(**kwargs)
         if errors:
             return None, errors
 
-        # Crea y persiste usando los argumentos desempaquetados
+        # si son correctos se crea el objeto en la RAM para devolverlo en el return y se lo persiste en la base de datos simultaneamente
         instance = cls.objects.create(**kwargs)
         return instance, []
 
+    #self porque es un metodo de instancia, el objeto ya existe
     def update(self, **kwargs) -> list[str]:
         """
         Sigue la estructura exacta del ejemplo: valida los nuevos datos 
         y si pasa, altera los atributos de la instancia y guarda.
         """
+        #como validate es un metodo de clase hay que llamarlo con el atributo __class__
         errors = self.__class__.validate(**kwargs)
         if errors:
             return errors
