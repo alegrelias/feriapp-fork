6f36ae35e399f36618a7390e4307b97aea54bd22
alegrelias26 <elias.n.alegre26-1@gmail.com>
2026-06-28 14:10:22 -0300

    Se agregan modelos faltantes al archivo admin (Resenia, Sector, Visitante) y se corrige bug menor

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,7 @@
 """Configuración del panel de administración para la app principal."""
 
 from django.contrib import admin
-from .models import Feria, Categoria, Emprendedor, Inscripcion 
+from .models import Feria, Categoria, Emprendedor, Inscripcion, Visitante, Resenia, Sector
 
 # TODO: reemplazar por @admin.register con list_display, list_filter, search_fields
 @admin.register(Feria)
@@ -34,4 +34,28 @@ class InscripcionAdmin(admin.ModelAdmin):
 
     list_filter = ('fecha_inscripcion', 'numero_puesto')
 
-    search_fields = ('feria', 'fecha_inscripcion')
+    search_fields = ('feria__nombre', 'fecha_inscripcion')
+
+@admin.register(Visitante)
+class VisitanteAdmin(admin.ModelAdmin):
+    list_display = list_display = ('nombre', 'apellido', 'email', 'usuario', 'fecha_registro')
+
+    list_filter = ('fecha_registro', 'email')
+
+    search_fields = ('nombre', 'fecha_registro')
+
+@admin.register(Resenia)
+class ReseniaAdmin(admin.ModelAdmin):
+    list_display = ('visitante', 'feria', 'calificacion', 'comentario', 'fecha_resenia')
+
+    list_filter = ('calificacion', 'feria__nombre', 'fecha_resenia')
+
+    search_fields = ('calificacion', 'feria__nombre')
+
+@admin.register(Sector)
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('feria', 'edicion', 'nombre', 'capacidad_puestos', 'tiene_conexion_electrica')
+
+    list_filter = ('feria__nombre', 'nombre', 'capacidad_puestos', 'tiene_conexion_electrica')
+
+    search_fields = ('nombre', 'capacidad_puestos')
